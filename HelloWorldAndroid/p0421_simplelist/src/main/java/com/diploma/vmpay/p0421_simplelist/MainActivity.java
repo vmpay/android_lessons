440f3126b68872e0e5241f3df0ea0063f9ea3078
vmpay <vereszp@gmail.com>
2016-02-09 23:04:55 +0100

    lesson 45

---
+++ HelloWorldAndroid/p0421_simplelist/src/main/java/com/diploma/vmpay/p0421_simplelist/MainActivity.java
@@ -0,0 +1,28 @@
+package com.diploma.vmpay.p0421_simplelist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь", "Анна", "Денис", "Андрей" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // находим список
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
+
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.my_list_view, names);
+
+        // присваиваем адаптер списку
+        lvMain.setAdapter(adapter);
+    }
+}
